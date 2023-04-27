4eddcad9d011100dd16e422e9a7f0d91c3b64f09
reniel8to3 <renielalmariomalabo@gmail.com>
2023-04-27 13:02:09 +0800

    pseudocode

---
+++ #Write a Python program that reads a tex.py
@@ -0,0 +1,5 @@
+#Write a Python program that reads a text file named numbers.txt that contains 20 integers. The program will create two other text file; the first text file will be named even.txt that will contain all even numbers extracted from numbers.txt. The second text file will be named odd.txt that will contain all odd numbers extracted from the numbers.txt.
+
+#read input file
+#define even numbers
+#define odd numbers
