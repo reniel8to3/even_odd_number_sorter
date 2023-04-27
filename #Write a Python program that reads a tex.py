bdb6508d626beddc60af557e58653e30e5279b75
reniel8to3 <renielalmariomalabo@gmail.com>
2023-04-27 13:38:06 +0800

    revision

--- #Write a Python program that reads a tex.py
+++ #Write a Python program that reads a tex.py
@@ -1,7 +1,8 @@
 #Write a Python program that reads a text file named numbers.txt that contains 20 integers. The program will create two other text file; the first text file will be named even.txt that will contain all even numbers extracted from numbers.txt. The second text file will be named odd.txt that will contain all odd numbers extracted from the numbers.txt.
 print ("This program is created to separate even and odd numbers from 20 integers.")
 #read input file
-with open ("numbers.txt", "r") as numbers_file:
+with open ("integers.txt", "r") as numbers_file:
+    integers = list(map(int, numbers_file.read().split()))
 #create two lists
 even_numbers = []
 odd_numbers = []
@@ -12,4 +13,3 @@ for num in integers:
     else:
         odd_numbers.append(num)
 #define even numbers
-#define odd numbers
